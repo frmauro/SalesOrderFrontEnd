bb15f3fa090930b59d3906ece7ec07bde5f091ea
frmauro <frmauro8@gmail.com>
2020-11-01 15:02:15 -0300

    implementation orderService

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from "rxjs";
+
+import { ORDERS } from "./models/mockOrder";
+import { Order } from "./models/Order";
+import { OrderStatus } from "./models/OrderStatus";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getOrders(): Order[]{
+    return ORDERS;
+  }
+
 }
