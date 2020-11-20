ac5de3ba0383c5786e4e9576ab881aa6c026f213
frmauro <frmauro8@gmail.com>
2020-11-20 12:44:16 -0300

    implementation LoginComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { EditComponent } from "./order/edit/edit.component";
 import { ProductsComponent } from "./product/list/list.component";
 import { LoginComponent } from "./login/login.component";
 
+import { AuthGuard } from "./guards/auth.guard";
+
 const routes: Routes = [
   //{ path: '', redirectTo: '/orders', pathMatch: 'full' },
-  { path: 'edit/:id', component: EditComponent },
-  { path: 'orders', component: ListComponent },
-  { path: 'create', component: CreateComponent },
-  { path: 'products', component: ProductsComponent },
+  { path: 'edit/:id', component: EditComponent, canActivate: [AuthGuard] },
+  { path: 'orders', component: ListComponent, canActivate: [AuthGuard] },
+  { path: 'create', component: CreateComponent, canActivate: [AuthGuard] },
+  { path: 'products', component: ProductsComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent},
 
   { path: '', redirectTo: '/login', pathMatch: 'full'}
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -13,12 +13,11 @@ export class AuthGuard implements CanActivate {
     ) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
-        // const currentUser = this.userService.currentUserValue;
-        // if (currentUser) {
-        //     //authorised so return true
-        //     return true;
-        // }
-
+        const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        if (currentUser) {
+            //authorised so return true
+            return true;
+        }
         this.router.navigate(['/login']);
         return false;
     }
