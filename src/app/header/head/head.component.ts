c9f372ed9aabd783e3e4dc155a57848cf692e050
frmauro <frmauro8@gmail.com>
2020-11-23 10:41:21 -0300

    implementation jwt token

--- src/app/header/head/head.component.ts
+++ src/app/header/head/head.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { UserService } from "../../user.service";
 import { User } from "../../models/User";
@@ -11,11 +12,16 @@ import { User } from "../../models/User";
 export class HeadComponent implements OnInit {
   currentUser: User;
 
-  constructor(private userService: UserService) {
+  constructor(private router: Router, private userService: UserService) {
       this.userService.currentUser.subscribe(x => this.currentUser = x);
    }
 
   ngOnInit(): void {
   }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/login']);
+}
 
 }
